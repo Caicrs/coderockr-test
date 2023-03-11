d61165cb456755336917a3db8f9bd49e9b77d4d4
caicrs <caicrs.contact.com>
2023-03-11 18:51:07 -0300

    Authentication created

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+    const { email, password } = loginDto;
+
+    // Procura e checa se o user existe, usando o nickname
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new UnauthorizedException('Usuário e/ou senha inválidos');
+    }
+
+    // Valida se a senha informada é corretaa
+    const isHashValid = await bcrypt.compare(password, user.password);
+
+    if (!isHashValid) {
+      throw new UnauthorizedException('Usuário e/ou senha inválidos');
+    }
+
+    delete user.password;
+
+    return {
+      token: this.jwtService.sign({ email }),
+      user,
+    };
+  }
+}
