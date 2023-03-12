8e3cc96afa9e168d6fcd7205c777c703d29ae699
caicrs <caicrs.contact.com>
2023-03-12 20:00:15 -0300

    General changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { InvestmentsListModule } from './investmentList/investmentsList.module';
+import { InvestmentModule } from './investments/investments.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, InvestmentModule,InvestmentsListModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
